6bef8baea2a67a372dc4fb642f25f981a5f58131
András Gajdács <drauthev@gmail.com>
2014-04-25 14:15:56 +0200

    setClient2 added

--- space_battle/src/interfaces/ServerForClient.java
+++ space_battle/src/interfaces/ServerForClient.java
@@ -1,7 +1,5 @@
 package interfaces;
 
-import java.util.SortedMap;
-
 public interface ServerForClient extends Runnable {
 	// public void run() 							// entry point
 	public void disconnect();						// lelép a kliens (vagy a network controller elveszti a kapcsolatot)
@@ -9,6 +7,7 @@ public interface ServerForClient extends Runnable {
 	public void startRequest();						// kliens kérvényezi a játék indítását
 	public void sendName(String name);				// játékosnév elküldése; szerver felírhatja a high scores-ba
 	public void terminate();						// zárul a program (csak client2-re hívja vissza!)
+	public void setClient2(ClientForServer c2);		// kereszthivatkozás feloldása miatt kell
 }
 
 // konstruktor: Server(GameType gt, GameSkill gs, Client_for_Server c1, Client_for_Server c2);
