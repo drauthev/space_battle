9b992a08bca3814aa1bb528d43556cbe18dd96c8
András Gajdács <drauthev@gmail.com>
2014-04-25 14:21:16 +0200

    completed, untested

--- space_battle/src/client/PlayerController.java
+++ space_battle/src/client/PlayerController.java
@@ -1,19 +1,81 @@
 package client;
 
 import java.awt.event.KeyEvent;
-import java.util.HashMap;
+import java.util.EnumMap;
+import java.util.TreeMap;
 
+import enums.PlayerAction;
 import interfaces.PlayerController_for_Client;
-import server.*;;
+import interfaces.ServerForPlayerController;;
 
 public class PlayerController implements PlayerController_for_Client {
-	boolean configure(HashMap<String, Character> bindings) { return false; }
-	void registerServer(Server sv) {}
+	interface Bridge {
+		void exec(boolean pressed);
+	}
 	
+	private ServerForPlayerController server;
+	private TreeMap<Integer, Bridge> bindings = new TreeMap<>();
+	
+	public void bindKey(PlayerAction action, Integer key) {
+		switch (action)
+		{
+			case P1FIRE: bindings.put(key, new Bridge(){
+				public void exec(boolean pressed) {
+					if (pressed) server.fire(0);
+					else server.releaseFire(0);
+				}
+			} );
+			break;
+			case P2FIRE: bindings.put(key, new Bridge(){
+				public void exec(boolean pressed) {
+					if (pressed) server.fire(1);
+					else server.releaseFire(1);
+				}
+			} );
+			break;
+			case P1LEFT: bindings.put(key, new Bridge(){
+				public void exec(boolean pressed) {
+					if (pressed) server.moveLeft(0);
+					else server.releaseLeft(0);
+				}
+			} );
+			break;
+			case P2LEFT: bindings.put(key, new Bridge(){
+				public void exec(boolean pressed) {
+					if (pressed) server.moveLeft(1);
+					else server.releaseLeft(1);
+				}
+			} );
+			break;
+			case P1RIGHT: bindings.put(key, new Bridge(){
+				public void exec(boolean pressed) {
+					if (pressed) server.moveRight(0);
+					else server.releaseRight(0);
+				}
+			} );
+			break;
+			case P2RIGHT: bindings.put(key, new Bridge(){
+				public void exec(boolean pressed) {
+					if (pressed) server.moveRight(1);
+					else server.releaseRight(1);
+				}
+			} );
+			break;
+		}
+	}
+
+	public PlayerController(ServerForPlayerController sv)
+	{
+		server = sv;
+	}
 	
 	@Override
-	public void dispatchKeyEvent(KeyEvent e) {
-		// TODO Auto-generated method stub
+	public void dispatchKeyEvent(KeyEvent e, boolean pressed) {
+		Bridge b = bindings.get(e.getKeyCode());
 		
+		if (b != null)
+		{
+			b.exec(pressed);
+		}
 	}
 }
